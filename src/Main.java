fe8fb5ae697a65972cd5a632b764e7dc86aab5c6
Anton Samolazov <sam_antoha@mail.ru>
2021-05-27 23:06:53 +1000

    first

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main (String[] args) {
+        int ticketPrice = 178020;
+        int milePrice = 20;
+
+        int milesAmount = ticketPrice / milePrice;
+        System.out.println(milesAmount);
+    }
+
+}
